422da6474facd3ec53d0dc7263adc54058ae9d63
thanhtunguet <thanhtung.uet@gmail.com>
2021-11-11 22:12:27 +0700

    add module css

--- react-native-css-module.d.ts
+++ react-native-css-module.d.ts
@@ -1,8 +1,15 @@
 declare module '*.scss' {
-    import type {StyleProp} from 'react-native';
+  import type {StyleProp} from 'react-native';
+
+  const styles: Record<string, StyleProp<any>>;
+
+  export default styles;
+}
   
-    const styles: Record<string, StyleProp<any>>;
-  
-    export default styles;
-  }
-  
+declare module '*.css' {
+  import type {StyleProp} from 'react-native';
+
+  const styles: Record<string, StyleProp<any>>;
+
+  export default styles;
+}
